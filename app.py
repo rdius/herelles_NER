5ffc68c2045271fc8b9bb3864226fe87c655b5a4
rdius <rdiuskaf@gmail.Com>
2023-07-22 17:35:34 +0200

    update app

--- app.py
+++ app.py
@@ -27,17 +27,21 @@ nlp = load_model()  # Load the model globally
 @st.cache_data
 def process_text(user_input):
     _doc = nlp(user_input)
-    return _doc.to_bytes(), json.dumps(_doc.to_json())  # Return the bytes and JSON representation
+    return _doc, _doc.to_bytes(), json.dumps(_doc.to_json())  # Return the bytes and JSON representation
 
-def handle_uploaded_file(file):
-    sections = file.read().decode("utf-8").split(">>>")
-    labelled_sections = []
-    for i, section in enumerate(sections, 1):
-        lines = section.strip().split('\n', 1)
-        if len(lines) > 1:
-            label, text = lines
-            labelled_sections.append((label.strip(), text.strip(), i))  # Also store segment number
-    return labelled_sections
+
+@st.cache_data
+def process_text___(user_input):
+    _doc = nlp(user_input)
+    segments_cache = {}  # Initialize an empty cache for storing segment-based entity dictionaries
+    for label, text, segment_number in labelled_sections:
+        if (label, segment_number) not in segments_cache:
+            _doc_bytes, _doc_json = _doc.to_bytes(), json.dumps(_doc.to_json())  # Return the bytes and JSON representation
+            _doc = spacy.tokens.Doc(nlp.vocab).from_bytes(_doc_bytes)  # Reconstruct the Doc object from bytes
+            segments_cache[(label, segment_number)] = get_entities_json(_doc, selected_labels)  # Cache entities
+    return _doc_bytes, _doc_json, segments_cache  # Return the segments cache along with bytes and JSON representation
+
+
 
 
 # Parse text into labelled sections
@@ -83,13 +87,64 @@ def get_table_download_entities_counter(entities_counter):
 
 
 
-def get_entities_json(doc, labels):
+def get_entities_json__(doc, labels):
     """
     Convert entity information to a dictionary
     """
     ent_dic = {ent.text: (ent.start_char, ent.end_char, ent.label_) for ent in doc.ents if ent.label_ in labels and len(ent.text)>2}
     print(ent_dic)
     return ent_dic# {ent.text: (ent.start_char, ent.end_char, ent.label_) for ent in doc.ents if ent.label_ in labels and len(ent.text)>2}
+
+
+# Modified get_entities_json() function
+def get_entities_json2(doc, labels):
+    """
+    Convert entity information to a dictionary
+    """
+    ent_dic = {}
+    for ent in doc.ents:
+        if ent.label_ in labels and len(ent.text)>2:
+            ent_dic.setdefault(ent.label_, {}).setdefault(ent.text, []).extend([ent.start_char, ent.end_char])
+    return ent_dic
+
+def get_entities_json2(doc, labels, segment_number):
+    """
+    Convert entity information to a dictionary with a nested structure {segment: {label: {entity: [start, end]}}}
+    """
+    ent_dict = {}
+    for ent in doc.ents:
+        if ent.label_ in labels and len(ent.text)>2:
+            if segment_number not in ent_dict:
+                ent_dict[segment_number] = {ent.label_: {ent.text: [ent.start_char, ent.end_char]}}
+            else:
+                if ent.label_ not in ent_dict[segment_number]:
+                    ent_dict[segment_number][ent.label_] = {ent.text: [ent.start_char, ent.end_char]}
+                else:
+                    if ent.text not in ent_dict[segment_number][ent.label_]:
+                        ent_dict[segment_number][ent.label_][ent.text] = [ent.start_char, ent.end_char]
+    return ent_dict
+
+def get_entities_json(doc, labels, segment_number):
+    """
+    Convert entity information to a dictionary with a nested structure {segment: {label: {entity: [line, start, end]}}}
+    """
+    ent_dict = {}
+    for ent in doc.ents:
+        if ent.label_ in labels and len(ent.text)>2:
+            line = doc[ent.start].sent.start
+            if segment_number not in ent_dict:
+                ent_dict[segment_number] = {ent.label_: {ent.text: [line, ent.start_char, ent.end_char]}}
+            else:
+                if ent.label_ not in ent_dict[segment_number]:
+                    ent_dict[segment_number][ent.label_] = {ent.text: [line, ent.start_char, ent.end_char]}
+                else:
+                    if ent.text not in ent_dict[segment_number][ent.label_]:
+                        ent_dict[segment_number][ent.label_][ent.text] = [line, ent.start_char, ent.end_char]
+    return ent_dict
+
+
+
+
 
 def display__(doc, selected_labels, segment_number):
     label_colors = {"Nomc_H6": "red", "Nomc_H5": "blue", "Nomc_H4": "green", "Nomc_H3": "purple", "Nomc_H2": "orange",
@@ -210,30 +265,31 @@ if __name__ == '__main__':
 
     entities_counter = Counter()
 
-    # Process all segments and cache the results
+
     for label, text, segment_number in labelled_sections:
         if (label, segment_number) not in segments_cache:
-            _doc_bytes, _doc_json = process_text(text)  # Process the text
+            #_doc_bytes, _doc_json = process_text(text)  # Process the text
+            _doc, _doc_bytes, _doc_json = process_text(text)
             _doc = spacy.tokens.Doc(nlp.vocab).from_bytes(_doc_bytes)  # Reconstruct the Doc object from bytes
             segments_cache[(label, segment_number)] = _doc
-            entities = get_entities_json(_doc, selected_labels)
-            print("****: ", entities)
+            entities = get_entities_json(_doc, selected_labels, segment_number)
             entities_cache[(label, segment_number)] = entities  # Cache entities
         else:
             _doc = segments_cache[(label, segment_number)]  # Retrieve the Doc object from cache
             entities = entities_cache[(label, segment_number)]  # Retrieve entities from cache
 
         if label == selected_segment:
-            #_doc = Doc(_doc.vocab, words=[t.text for t in _doc], spaces=[t.whitespace_ for t in _doc])
-
             display(_doc, selected_labels, segment_number)
-            entities_counter.update([ent[2] for ent in entities.values()])  # Increment the count of each entity label
-            #st.write("entities_counter :",  entities_counter)
+
+            # Update entities_counter
+            for label, entity_dict in entities[segment_number].items():
+                entities_counter[label] += len(entity_dict)
 
     fig = go.Figure(data=[go.Pie(labels=list(entities_counter.keys()),
                              values=list(entities_counter.values()),
                              marker_colors=[labels[key] for key in entities_counter.keys()],
-                             hole=.3,
+
+                             hole=.3
                              )
                    ])
 
